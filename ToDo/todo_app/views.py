903ad30855464ebcf7f4cfec511237d9a5922faf
vavsar <sarcigar@gmail.com>
2021-03-07 13:47:04 +0400

    feat: add user registration by login/password feat: include nav panel with login/logout button fix: fix some templates, add simple model to check registration

--- ToDo/todo_app/views.py
+++ ToDo/todo_app/views.py
@@ -1,2 +1,12 @@
+from django.views.generic import ListView
 
+from .models import Task
+
+
+class IndexPage(ListView):
+    template_name = 'todo_app/index.html'
+    model = Task
+    queryset = Task.objects.all()
+    context_object_name = 'tasks'
+    paginate_by = 10
 
