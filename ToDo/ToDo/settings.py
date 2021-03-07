a92b6865fdd8fb17813a06bdf62f77181c45b8d6
vavsar <sarcigar@gmail.com>
2021-03-07 13:30:13 +0400

    feat: add user registration by login/password feat: include nav panel with login/logout button

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -10,13 +10,13 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
-    'todo_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app',
 ]
 
 MIDDLEWARE = [
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'ToDo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('', include('todo_app.urls'))
 ]
