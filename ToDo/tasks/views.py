7cc9f206789d789c79eb7f39f5023fd02b856e47
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-16 10:48:42 +0300

    feat: add view-task view

--- ToDo/tasks/views.py
+++ ToDo/tasks/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Task
 
@@ -24,3 +26,11 @@ class CreateTask(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+
+@login_required
+def view_task(request, username, post_id):
+    task = get_object_or_404(Task, id=post_id, owner__username=username)
+    if task.owner == request.user:
+        return render(request, 'tasks/single_task.html', {'task': task})
+    return redirect('index')
