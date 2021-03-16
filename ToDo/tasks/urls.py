f4c284dc0c7a933b2f636eee65d3ef6b0404bb35
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-16 10:45:44 +0300

    feat: add url to urlpatterns

--- ToDo/tasks/urls.py
+++ ToDo/tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.TaskList.as_view(), name='index'),
     path('add-task/', views.CreateTask.as_view(),
          name='add-task'),
+    path('<str:username>/<int:post_id>/', views.view_task, name='view-task'),
 ]
