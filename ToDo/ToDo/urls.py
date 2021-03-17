414005ca08126cd8a2c007b1b43e438abaf37956
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-17 21:48:09 +0300

    fix: remove handler from urls

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import handler403
-
-handler403 = 'tasks.views.forbidden' # noqa
 
 urlpatterns = [
     path('auth/', include('users.urls')),
