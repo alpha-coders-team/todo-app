0175157c69852ac78506b7638a8f36a268ff622e
vavsar <sarcigar@gmail.com>
2021-03-29 23:14:04 +0400

    feat: add account delete button to nav panel

--- ToDo/tasks/urls.py
+++ ToDo/tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='index'),
-    path('delete-user/', views.UserDelete.as_view()),
+    path('delete-user/', views.UserDelete.as_view(), name='user_delete'),
     path('add-task/', views.CreateTask.as_view(),
          name='add-task'),
     path('<str:owner>/<int:pk>/', views.TaskDetailView.as_view(),
