1f1368d10b2d498b7eedde495c5ea10d5fb05f74
vavsar <sarcigar@gmail.com>
2021-03-29 22:36:11 +0400

    delete empty todo_app folder fix login redirect error

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -10,7 +10,6 @@ ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
-    'todo_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -98,6 +97,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 SITE_ID = 1
